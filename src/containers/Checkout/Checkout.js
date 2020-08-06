df16a2cbed7738675a49156987e430dfe9b98597
HenryRivera <henry.rivera@nyu.edu>
2020-08-06 16:52:16 -0400

    navigating to the contact data component

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary'
+import ContactData from './ContactData/ContactData'
 
 class Checkout extends Component {
     // checkout summary form
@@ -39,6 +41,7 @@ class Checkout extends Component {
                     ingredients={this.state.ingredients}
                     cancelCheckout={this.cancelCheckoutHandler}
                     continueCheckout={this.continueCheckoutHandler}/>
+                <Route path={this.props.match.path + '/contact-data'} component={ContactData}/>
             </div>
         )
     }
