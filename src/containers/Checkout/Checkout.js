a31bda68f8ce7eec503e7ea7372c60daf4ad5f8f
HenryRivera <henry.rivera@nyu.edu>
2020-08-06 14:41:10 -0400

    Implementing Routing feature on app

---
+++ src/containers/Checkout/Checkout.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Checkout extends Component {
+    // checkout summary form
+
+
+    // button to continue
+
+    // button to cancel checkout process
+}
+
+export default Checkout
